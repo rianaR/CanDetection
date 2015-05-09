f40faeb21076475d37c6be528c7da1c010092c01
Riana Rabehasy <rabehasy@polytech.unice.fr>
2015-05-09 22:46:06 +0200

    Recherche de l'ellipse du haut de la canette (fonctionne avec toutes nos images)

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ int max_thresh = 255;
 RNG rng(12345);
 Point h_g,b_d;
 Mat dst;
+double INFINI=1000.0;
 
 // Image
 void thresh_callback(int, void*);
@@ -21,55 +22,8 @@ double length(Vec4i line) {
     return sqrt(pow(line[3]-line[1],2)+pow(line[2]-line[0],2));
 }
 
-void thresh_callback(int, void*)
-{
-
-  Mat threshold_output;
-  vector<vector<Point> > contours;
-  vector<Vec4i> hierarchy;
-
-  /// Detect edges using Threshold
-  threshold( src_gray, threshold_output, thresh, 255, THRESH_BINARY );
-  /// Find contours
-  findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-
-
-  /// Find the rotated rectangles and ellipses for each contour
-  vector<RotatedRect> minRect( contours.size() );
-  vector<RotatedRect> minEllipse( contours.size() );
-
-
-    for( int i = 0; i < contours.size(); i++ )
-    {
-        minRect[i] = minAreaRect( Mat(contours[i]) );
-        if( contours[i].size() > 5 )
-        {
-            minEllipse[i] = fitEllipse( Mat(contours[i]) );
-        }
-    }
-
-    /// Draw contours + rotated rects + ellipses
-    Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
-    for( int i = 0; i< contours.size(); i++ )
-    {
-        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        // contour
-        drawContours( drawing, contours, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
-        // ellipse
-        ellipse( drawing, minEllipse[i], color, 2, 8 );
-        // rotated rectangle
-        Point2f rect_points[4]; minRect[i].points( rect_points );
-        /*
-        for( int j = 0; j < 4; j++ )
-          line( drawing, rect_points[j], rect_points[(j+1)%4], color, 1, 8 );
-          */
-    }
-
-    /// Show in a window
-    namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-    imshow( "Contours", drawing );
-}
-
+/// Renvoie les points haut gauche et bas droite qui définissent le rectangle qui contient la canette
+/// Le rectangle n'est pas orienté, il est vertical.
 vector<Point2i> findCanLimits(Mat &contoursImg) {
 
     /// Délimitations de la canette
@@ -103,24 +57,171 @@ vector<Point2i> findCanLimits(Mat &contoursImg) {
     return limits;
 }
 
+///Associe des points au contour de la canette.
+vector<Point2i> fitCanShape(Mat &can, int nbPoints) {
+    vector<Point2i> points;
+    //Bas gauche -> haut gauche
+    for (int i=nbPoints-1; i>=0; i--) {
+        uchar *row = can.ptr<uchar>(i*can.rows/nbPoints);
+        int col=0;
+        int val = (int) row[col];
+        while ((val==0) && (col < can.cols)) {
+            col++;
+            val = (int) row[col];
+        }
+        if (col!=can.cols) {
+            points.push_back(Point2i(col,i*can.rows/nbPoints));
+        }
+        else {
+            points.push_back(Point2i(can.cols,i*can.rows/nbPoints));
+        }
+    }
+
+    //Haut droite -> bas droite
+    for (int i=0; i<nbPoints; i++) {
+        uchar *row = can.ptr<uchar>(i*can.rows/nbPoints);
+        int col=can.cols;
+        int val = (int) row[col];
+        while ((val==0) && (col >= 0)) {
+            col--;
+            val = (int) row[col];
+        }
+        if (col!=0) {
+            points.push_back(Point2i(col,i*can.rows/nbPoints));
+        }
+        else {
+            points.push_back(Point2i(can.cols,i*can.rows/nbPoints));
+        }
+    }
+
+    return points;
+}
+
+///Donne les paramètres de la droite qui correspond le mieux aux points donnés
+///(c'est surtout la pente suivant x qui est significative)
+Vec4f slope(vector<Point2i> shape, int position, int nbPoints) {
+    vector<Point2i> pointsForSlope;
+    Vec4f segment;
+    for (int i=position-nbPoints/2; i <= position+nbPoints/2; i++) {
+        pointsForSlope.push_back(shape[i]);
+    }
+    fitLine(pointsForSlope,segment,CV_DIST_L12,0,0.01,0.01);
+    return segment;
+}
+
+///Retourne l'index dans le vecteur shape du point correspondant au
+///coin supérieur haut de la canette.
+int topLeftOfCan(vector<Point2i> &shape, int resolution) {
+    int position=resolution/2;
+
+    ///Calcul du coeff directeur du segment gauche de la canette
+    Vec4f segment=slope(shape,position, 6);
+    //Vec4i currentSegment;
+    while (abs(segment[0]) < 0.2) {
+        position++;
+        segment=slope(shape, position, 6);
+        cout << "segment : " << "vx=" << segment[0] << " vy=" << segment[1] << endl;
+    }
+
+    while (abs(segment[0]) > 0.2) {
+        position++;
+        segment=slope(shape,position, 3);
+        cout << "segment : " << "vx=" << segment[0] << "vy=" << segment[1] << endl;
+    }
+    return ++position;
+}
+
+///Recherche l'ellipse du haut de la canette
+vector<Point2i> topEllipseOfCan(vector<Point2i> &shape, int resolution) {
+    int position = topLeftOfCan(shape, resolution);
+
+    int topY=shape[++position].y;
+    cout << "topY : " << topY << endl;
+    //Index du haut de la canette trouvé
+    vector<Point2i> halfEllipse;
+    for (int currentY=topY; ((currentY <= topY) && (position < shape.size())); position++) {
+        currentY=shape[position].y;
+        halfEllipse.push_back(shape[position]);
+    }
+    cout << "Nombre de points de l'ellipse : " << halfEllipse.size() << endl;
+    if (position < shape.size()) {
+        return halfEllipse;
+    }
+    return vector<Point2i>();
+}
+
+//Haut gauche -> haut droite
+//    for (int i=0; i<nbPoints; i++) {
+//        Mat col=can.col(i*can.cols/nbPoints);
+//        int row=0;
+//        int val=(int) col.at<uchar>(row);
+//        while ((val==0) && (row < can.rows)) {
+//            row++;
+//            val = (int) col.at<uchar>(row);
+//        }
+//        if (row != can.rows) {
+//            points.push_back(Point2i(i*can.cols/nbPoints,row));
+//        }
+//        else {
+//            points.push_back(Point2i(i*can.cols/nbPoints,0));
+//        }
+//    }
+
 int main(int argc, char *argv[])
 {
+    cout.precision(4);
 
-    Mat img = imread("images/cans/perrier.jpg", CV_LOAD_IMAGE_COLOR);
+    Mat img = imread("images/cans/fanta.jpg", CV_LOAD_IMAGE_COLOR);
+
     if(img.empty())
        return -1;
 
+    /// Image couleur pour pouvoir tracer des formes en couleur
+    /// sur l'image des contours qui est en noir et blanc
     Mat cdst;
+    /// Tracé des contours
     Canny(img, dst, 50, 200, 3);
+    namedWindow("canBefore",CV_WINDOW_AUTOSIZE);
+    imshow("canBefore", dst);
+    /// Floutage, utilisé pour rendre les bords plus continus
     GaussianBlur(dst,dst,Size(3,3),0);
+
+    threshold( dst, dst, thresh, 255, THRESH_BINARY );
+    namedWindow("canAfter",CV_WINDOW_AUTOSIZE);
+    imshow("canAfter", dst);
     cvtColor(dst, cdst, CV_GRAY2BGR);
 
-
+    /// On trace le rectangle qui contient la canette
     vector<Point2i> limits;
-    cout << "lol" << endl;
     limits = findCanLimits(dst);
+    /// Le rectangle est défini par 2 points contenus dans limits.
     Rect limitsRect(limits[0],limits[1]);
     rectangle(cdst,limitsRect,Scalar(255,0,0));
+
+    /// Can : image contenant le rectangle qui englobe la canette
+    Mat can(dst, limitsRect);
+    Mat colorCan;
+    cvtColor(can,colorCan,CV_GRAY2BGR);
+
+    /// On cherche la forme qui s'adaptera le mieux au contour extérieur de la canette
+    vector<Point2i> shape;
+
+    /// Nombre de points par coté à fitter sur le contour
+    int resolution = 60;
+    shape = fitCanShape(can,resolution);
+
+    /// On trouve le coté gauche du "couvercle de la canette"
+    int topLeft = topLeftOfCan(shape, resolution);
+    cout << topLeft << endl;
+    circle(colorCan,Point(shape[topLeft].x,shape[topLeft].y), 5, Scalar(200,100,50));
+    imshow("Top Left",colorCan);
+
+    /// On récupère la demi-ellipse du haut
+    vector<Point2i> halfEllipse = topEllipseOfCan(shape, resolution);
+    for (int i=0;i<halfEllipse.size()-1;i++) {
+        line(colorCan,halfEllipse[i],halfEllipse[i+1],Scalar(150,30,59), 3, CV_AA);
+    }
+    imshow("halfEllipse",colorCan);
 
     /// Méthode utilisant la transformée de Hough
     /// Pas très efficace pour trouver les lignes verticales des bords
@@ -190,8 +291,9 @@ int main(int argc, char *argv[])
         /// Detect edges using Threshold
         threshold( dst, threshold_output, thresh, 255, THRESH_BINARY );
         /// Find contours
-        Rect searchEllipseArea(limitsRect.x,limitsRect.y,limitsRect.width,limitsRect.height/3);
+        Rect searchEllipseArea(limitsRect.x,limitsRect.y,limitsRect.width,limitsRect.height/6);
         Mat area(img, searchEllipseArea);
+
         findContours( Mat(threshold_output, searchEllipseArea), contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
         Mat drawing = Mat::zeros( dst.size(), CV_8UC3 );
         for( int i = 0; i< contours.size(); i++ )
@@ -269,7 +371,7 @@ int main(int argc, char *argv[])
 
       /// Display
       namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
-      imshow("calcHist Demo", histImage );
+      //imshow("calcHist Demo", histImage );
 
     waitKey(0);
 
